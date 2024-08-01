4e45d681aa97bc47ef8ef329a58fb214fc0ae21e
Tisha-Coding <tishacoding@gmail.com>
2024-07-31 23:35:15 -0700

    DoubleClick Event

---
+++ doubleclickEvent/script.js
@@ -0,0 +1,9 @@
+function hey()
+{
+    document.getElementById("hello").innerText = "You have clicked Me!";
+    document.getElementById("hello").style.color = "navy";
+    document.getElementById("hello").style.padding = "30px";
+    document.getElementById("hello").style.border = "2px solid black";
+    document.getElementById("hello").style.backgroundColor = "lightgreen";
+    document.body.style.backgroundColor = "lightyellow";
+}
