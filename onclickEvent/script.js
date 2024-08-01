a10663a32501c8cf8bfb109bc0060346610f5f6b
Tisha-Coding <tishacoding@gmail.com>
2024-07-31 23:36:41 -0700

    OnClick Event

---
+++ onclickEvent/script.js
@@ -0,0 +1,7 @@
+function hey()
+{
+    document.getElementById("hello").innerText = "You have clicked Me!";
+    document.getElementById("hello").style.color = "navy";
+    document.getElementById("hello").style.padding = "30px";
+    document.getElementById("hello").style.border = "2px solid brown";
+}
